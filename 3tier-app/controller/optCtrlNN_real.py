7df05a69e7770bb04f24a880d2696d591fe5d551
emilio <emilio@mpbwalt.local>
2021-09-28 17:27:55 +0200

    reorganized  ctrl

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -55,7 +55,7 @@ def mitigateBottleneck(S, X, tgt):
 
 def genAfa():
     # r=np.random.choice([.1,.2,.3,.4,.5,.6,.7,.8,.9,1],p=[0.0182,0.0364,0.0545,0.0727,0.0909,0.1091,0.1273,0.1455,0.1636,0.1818])
-    r = np.random.choice([.3, .4, .5, .6, .7, .8, .9, 1])
+    r = np.random.choice([.1, .2, .3, .4, .5, .6, .7, .8, .9, 1])
     return np.round(np.random.rand() * 0.1 + (r - 0.1), 4)
     # return np.round(np.random.rand()*0.3+0.7,4)
 
