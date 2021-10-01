95641abe9efcce87fc5a31ccc058ec070d8f9bc3
emilio <emilio@mpbwalt.local>
2021-10-01 19:49:56 +0200

    minor

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -138,8 +138,8 @@ def setU(optS):
     print("tier2","%dm"%(int(np.round(optS[2]*1000))))
     print(tier1.spec.template.spec.containers[0].resources)
     print(tier2.spec.template.spec.containers[0].resources)
-    tier1.spec.template.spec.containers[0].resources.limits.cpu="%dm"%(int(np.round(optS[1]*1000)))
-    tier2.spec.template.spec.containers[0].resources.limits.cpu="%dm"%(int(np.round(optS[2]*1000)))
+    tier1.spec.template.spec.containers[0].resources.limits["cpu"]="%dm"%(int(np.round(optS[1]*1000)))
+    tier2.spec.template.spec.containers[0].resources.limits["cpu"]="%dm"%(int(np.round(optS[2]*1000)))
     apps_api.patch_namespaced_deployment(name=tier1.metadata.name, namespace=tier1.metadata.namespace, body=tier1)
     apps_api.patch_namespaced_deployment(name=tier2.metadata.name, namespace=tier2.metadata.namespace, body=tier2)
     
