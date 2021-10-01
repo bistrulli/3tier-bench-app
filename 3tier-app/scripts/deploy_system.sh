24144d27af91f0783d0867676358044cea377114
emilio <emilio@mpbwalt.local>
2021-10-01 17:40:48 +0200

    addded kuberneted python interface

--- 3tier-app/scripts/deploy_system.sh
+++ 3tier-app/scripts/deploy_system.sh
@@ -8,7 +8,7 @@ sleep 2
 
 kubectl apply -f ../tier1/deploy.yaml
 kubectl apply -f ../tier2/deploy.yaml
-kubectl apply -f ../controller/deploy.yaml
+#kubectl apply -f ../controller/deploy.yaml
 kubectl apply -f ../services/tier1.yaml
 kubectl apply -f ../services/tier2.yaml
 kubectl apply -f ../services/monitor-ext.yaml
