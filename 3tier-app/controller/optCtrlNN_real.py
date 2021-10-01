9b0d4195e0402f4ad53393089b643296fab884e1
emilio <emilio@mpbwalt.local>
2021-10-01 18:44:12 +0200

    addded kuberneted python interface

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -136,8 +136,8 @@ def setU(optS):
     #         "path": "/spec/template/spec/containers/0/resources/limits/cpu"}]
     tier1.spec.template.spec.containers[0].resources.limits.cpu="%dm"%(int(np.round(optS[1]*1000)))
     tier2.spec.template.spec.containers[0].resources.limits.cpu="%dm"%(int(np.round(optS[2]*1000)))
-    apps_api.patch_namespaced_deployment(name=tier1.metadata.name, namespace=tier1.metadata.namespace, body=t1_patch, async_req=False)
-    apps_api.patch_namespaced_deployment(name=tier2.metadata.name, namespace=tier2.metadata.namespace, body=t2_patch, async_req=False)
+    apps_api.patch_namespaced_deployment(name=tier1.metadata.name, namespace=tier1.metadata.namespace, body=tier1, async_req=False)
+    apps_api.patch_namespaced_deployment(name=tier2.metadata.name, namespace=tier2.metadata.namespace, body=tier2, async_req=False)
     
 
 
