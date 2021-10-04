e82185db105926fc81d75937783f58ae9aa3fab3
emilio <emilio@mpbwalt.local>
2021-10-04 14:06:01 +0200

    add docker python

--- 3tier-app/controller/pydocker.py
+++ 3tier-app/controller/pydocker.py
@@ -9,8 +9,8 @@ r.close()
 client = docker.from_env()
 
 client.containers.run(image="bistrulli/client:0.1",
-                      command=["java","-Xmx4G","-jar","client-0.0.1-SNAPSHOT-jar-with-dependencies.jar","--initPop","20"
-                       ,"--queues",'["think", "e1_bl", "e1_ex", "t1_hw", "e2_bl", "e2_ex", "t2_hw"]',"--jedisHost","monitor"],
+                      command="java -Xmx4G -jar client-0.0.1-SNAPSHOT-jar-with-dependencies.jar --initPop 20\
+                       --queues '[\"think\" \"e1_bl\" \"e1_ex\" \"t1_hw\" \"e2_bl\" \"e2_ex\" \"t2_hw\"]' --jedisHost monitor",
                       auto_remove=True,
                       detach=False,
                       hostname="client")
