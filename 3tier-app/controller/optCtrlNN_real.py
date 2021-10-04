3142ef9609be59b2e90473d712a884a8a45b1b72
emilio <emilio@mpbwalt.local>
2021-10-04 16:54:23 +0200

    add docker python

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -320,7 +320,7 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 2
+    rep = 10
     sTime = 500
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
