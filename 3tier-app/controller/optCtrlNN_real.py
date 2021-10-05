56c9ed0c0fe32c7d2f1a5cba4bb09ab6001b7bb7
emilio <emilio@mpbwalt.local>
2021-10-05 09:20:59 +0200

    minot

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -324,7 +324,7 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 2
+    rep = 10
     sTime = 500
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
