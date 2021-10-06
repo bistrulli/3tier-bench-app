65ace8e48d995a5e443feb5e620e05285c8de48c
emilio <emilio@mpbwalt.local>
2021-10-06 23:47:17 +0200

    bug fix

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -182,10 +182,10 @@ def resetU():
 def getstate(r, keys, N):
     str_state=r.mget(keys)
     try:
-        astate = [float(str_state[0])]
+        astate = [max(float(str_state[0]),0)]
         gidx = 1;
         for i in range(1, N):
-            astate.append(float(str_state[gidx]) + float(str_state[gidx + 1]))
+            astate.append(max(float(str_state[gidx]) + float(str_state[gidx + 1]),0))
             if(float(str_state[gidx])<0 or float(str_state[gidx + 1])<0):
                 raise ValueError("Error! state < 0")
             gidx += 3
