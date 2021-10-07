e21ae33ff11d78e3f3189dd808a6ef86288b2ac8
emilio <emilio@mpbwalt.local>
2021-10-07 15:51:51 +0200

    install specific docker version

--- 3tier-app/scripts/install-docker.sh
+++ 3tier-app/scripts/install-docker.sh
@@ -15,7 +15,7 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt-get update -y
-sudo apt-get install docker-ce=5:20.10.8~3-0~ubuntu-bionic \ 
-					 docker-ce-cli=5:20.10.8~3-0~ubuntu-bionic containerd.io -y
+sudo apt-get install docker-ce=20.10.8~3-0~ubuntu-bionic \ 
+					 docker-ce-cli=20.10.8~3-0~ubuntu-bionic containerd.io -y
 
 sudo usermod -aG docker $USER
