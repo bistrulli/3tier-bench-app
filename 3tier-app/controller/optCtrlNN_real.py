5f8e8defe65f2e915b8fc6db820c0671c959b352
emilio <emilio@mpbwalt.home>
2021-10-10 23:12:51 +0200

    ..

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -350,7 +350,7 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 1
+    rep = 10
     sTime = 500
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
@@ -389,11 +389,11 @@ if __name__ == "__main__":
                 # compute ODE
                 if step == 0 or step % sTime == 0: 
                     Sold = None       
-                    #alfa.append(genAfa())
-                    alfa.append(1.0)
-                    #XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
+                    alfa.append(genAfa())
+                    #alfa.append(1.0)
+                    XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
                     #XSSIM[:, step] = getstate(r, keys, N)
-                    XSSIM[:, step] = [90, 0, 0]
+                    #XSSIM[:, step] = [90, 0, 0]
                     print(alfa[-1], XSSIM[:, step])
                     # print(XSSIM[:, step])
                     XSSIM2[:, step] = XSSIM[:, step]
