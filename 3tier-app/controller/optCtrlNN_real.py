41fe0335d21c51284ce54488b07e4097e4350fd6
emilio <emilio@mpbwalt.local>
2021-10-14 10:33:04 +0200

    minor

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -185,7 +185,7 @@ def startClient(initPop):
                           auto_remove=True,
                           detach=True,
                           hostname="client",
-                          #network="3tier-app_default",
+                          network="3tier-app_default",
                           stop_signal="SIGINT")
 
 
