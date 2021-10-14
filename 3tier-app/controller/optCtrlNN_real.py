d2a4a919c87046047a26f94147685f8ff0fc2664
emilio <emilio@mpbwalt.local>
2021-10-14 09:58:08 +0200

    minor

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -390,11 +390,11 @@ if __name__ == "__main__":
                      "%s/../learnt_model/open_loop_3tier_H5.mat" % (os.path.dirname(curpath)))
     
     isAR = True
-    isCpu = False
+    isCpu = True
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 10
+    rep = 1
     sTime = 500
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
