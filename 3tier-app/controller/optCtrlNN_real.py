189419f05f037b03bd72e38087dd77c44a40fcb1
emilio <emilio@mpbwalt.local>
2021-10-15 12:07:25 +0200

    minor

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 1
+    rep = 10
     sTime = 500
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
@@ -444,9 +444,9 @@ if __name__ == "__main__":
                     Sold = None       
                     #alfa.append(genAfa())
                     alfa.append(1.0)
-                    #XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
+                    XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
                     #XSSIM[:, step] = getstate(r, keys, N)
-                    XSSIM[:, step] = [100, 0, 0]
+                    #XSSIM[:, step] = [100, 0, 0]
                     print(alfa[-1], XSSIM[:, step])
                     # print(XSSIM[:, step])
                     XSSIM2[:, step] = XSSIM[:, step]
