0494c1ee658f3060940a1d11bf76e52fbae3ac3f
emilio <emilio@mpbwalt.local>
2021-10-21 13:27:41 +0200

    bug fix

--- 3tier-app/controller/docker_sys.py
+++ 3tier-app/controller/docker_sys.py
@@ -21,7 +21,7 @@ class dockersys(system_interface):
         r=Client("localhost:11211")
         r.set("stop","0")
         
-        self.client_cnt=self.dck_client.containers.run(image="bistrulli/client:0.7",
+        self.client_cnt=self.dck_client.containers.run(image="bistrulli/client:0.8",
                               command="java -Xmx4G -jar client-0.0.1-SNAPSHOT-jar-with-dependencies.jar --initPop %d --queues \
                                       '[\"think\", \"e1_bl\", \"e1_ex\", \"t1_hw\", \"e2_bl\", \"e2_ex\", \"t2_hw\"]' \
                                        --jedisHost monitor"%(initPop),
