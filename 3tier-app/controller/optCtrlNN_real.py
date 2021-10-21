74836e24cf0243064662cf0bd40324591362c8f8
emilio <emilio@mpbwalt.local>
2021-10-21 17:46:18 +0200

    ...

--- 3tier-app/controller/optCtrlNN_real.py
+++ 3tier-app/controller/optCtrlNN_real.py
@@ -472,8 +472,8 @@ if __name__ == "__main__":
                     drep+=1
                     
                     Sold = None       
-                    alfa.append(genAfa())
-                    #alfa.append(1.0)
+                    #alfa.append(genAfa())
+                    alfa.append(1.0)
                     #XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
                     XSSIM[:, step] = getstate(r, keys, N)
                     #XSSIM[:, step] = [100, 0, 0]
