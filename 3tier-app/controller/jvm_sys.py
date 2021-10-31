1a1c99a4ba9a1c3bfc64c658b936a82bbf1c04c1
emilio <emilio@mpbwalt.local>
2021-10-31 12:18:43 +0100

    created jvm sys

--- 3tier-app/controller/jvm_sys.py
+++ 3tier-app/controller/jvm_sys.py
@@ -68,7 +68,6 @@ class jvm_sys(system_interface):
     
     def initCgroups(self):        
         out=subprocess.check_call(["sudo","cgget", "-g", "cpu:t1"])
-        out.decode("UTF-8")
         if(out.find("Cgroup does not exist")==-1):
             subprocess.check_output(["sudo","cgcreate","-g","cpu:t1"])
         
