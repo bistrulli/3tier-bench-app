416f76285784c07ee12bac99cd4b6623061ef3cd
emilio <emilio@mpbwalt.local>
2021-11-04 16:07:26 +0100

    minor

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -349,7 +349,7 @@ if __name__ == "__main__":
                 optSNN[:, step] = optU[0:N]
                 tgtStory += [tgt]
                 
-                #time.sleep(0.3)
+                time.sleep(0.5)
                 
                 # optSPID[:,step]=optSPid
                 # optSPid=mitigateBottleneck(optSPid, Xsim3, tgt)
