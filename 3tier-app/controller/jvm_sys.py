88de83a37b821c5f142ed2ff318e0c6598c6a391
emilio <emilio@mpbwalt.local>
2021-11-04 17:05:14 +0100

    minor

--- 3tier-app/controller/jvm_sys.py
+++ 3tier-app/controller/jvm_sys.py
@@ -90,7 +90,7 @@ class jvm_sys(system_interface):
             self.waitTier1()
             self.sys.append(self.findProcessIdByName("tier1-0.0.1")[0])
         else:
-            subprocess.Popen(["cgexec", "-g", "cpu:t2", "--sticky", javaCmd, "-Xmx4G",
+            subprocess.Popen(["sudo","cgexec", "-g", "cpu:t2", "--sticky", javaCmd, "-Xmx4G",
                              "-Djava.compiler=NONE", "-jar",
                              '%stier2/target/tier2-0.0.1-SNAPSHOT-jar-with-dependencies.jar' % (self.sysRootPath),
                              '--cpuEmu', '%d' % (cpuEmu), '--jedisHost', 'localhost'])
@@ -98,7 +98,7 @@ class jvm_sys(system_interface):
             self.sys.append(self.findProcessIdByName("tier2-0.0.1")[0])
             
             
-            subprocess.Popen(["cgexec", "-g", "cpu:t1", "--sticky", javaCmd, "-Xmx4G",
+            subprocess.Popen(["sudo","cgexec", "-g", "cpu:t1", "--sticky", javaCmd, "-Xmx4G",
                              "-Djava.compiler=NONE", "-jar",
                              '%stier1/target/tier1-0.0.1-SNAPSHOT-jar-with-dependencies.jar' % (self.sysRootPath),
                              '--cpuEmu', "%d" % (cpuEmu), '--jedisHost', 'localhost',
