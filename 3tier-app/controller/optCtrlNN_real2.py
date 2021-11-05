4ec72a5aaa5bb1aad85410e7180db8f0dc0aeb73
emilio <emilio@mpbwalt.local>
2021-11-05 16:55:42 +0100

    minor

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -182,7 +182,7 @@ class optCtrlNN3:
             for ui in range(1, P.shape[0]):
                 ru += (uvar_dn[ui] - Sold[ui]) ** 2
         
-        model.minimize(obj + 0.15*ru + 0.15*casadi.sumsqr(uvar_dn[1:3]))
+        model.minimize(obj + 0.1*ru + 0.15*casadi.sumsqr(uvar_dn[1:3]))
         
         optionsIPOPT = {'print_time':False, 'ipopt':{'print_level':0}}
         optionsOSQP = {'print_time':False, 'osqp':{'verbose':False}}
@@ -206,9 +206,9 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 5
+    rep = 10
     drep = 0
-    sTime = 1000
+    sTime = 1200
     TF = sTime * rep * dt;
     Time = np.linspace(0, TF, int(np.ceil(TF / dt)) + 1)
     XSNN = np.zeros([N, len(Time)])
