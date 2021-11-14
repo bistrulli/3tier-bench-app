9496d64f54f07de8fc37e2d33fb8b2cdc6b4fc47
emilio <emilio@mpbwalt.local>
2021-11-14 14:00:00 +0100

    minor

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -267,8 +267,8 @@ if __name__ == "__main__":
                     drep+=1
                     
                     Sold = None       
-                    alfa.append(genAfa())
-                    #alfa.append(1)
+                    #alfa.append(genAfa())
+                    alfa.append(1)
                     #XSSIM[:, step] = [np.random.randint(low=10, high=100), 0, 0]
                     XSSIM[:, step] = plant.getstate(r)[0]
                     #XSSIM[:, step] = [100, 0, 0]
