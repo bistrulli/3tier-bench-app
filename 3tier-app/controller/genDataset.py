a0898674346bd6a3f2c78209912a434746aa3547
emilio <emilio@mpbwalt.local>
2021-11-22 22:59:48 +0100

    minor

--- 3tier-app/controller/genDataset.py
+++ 3tier-app/controller/genDataset.py
@@ -159,7 +159,7 @@ try:
                 if(isCpu):
                     dck_sys.setU(optS[0,1], "tier1")
                     dck_sys.setU(optS[0,2], "tier2")
-        time.sleep(0.5)
+        time.sleep(3)
         
     #salvo risultati intermedi
     Path(str(script_dir)+"/../data/%s/"%(str(myuuid)) ).mkdir( parents=True, exist_ok=True )
