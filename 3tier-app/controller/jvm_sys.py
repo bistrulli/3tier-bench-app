5af877116dcfee0a1a9857b84805288448c1b283
emilio <emilio@mpbwalt.local>
2021-11-22 22:32:21 +0100

    minor

--- 3tier-app/controller/jvm_sys.py
+++ 3tier-app/controller/jvm_sys.py
@@ -76,7 +76,7 @@ class jvm_sys(system_interface):
         
         if(self.isCpu):
             subprocess.Popen([javaCmd, "-Xmx6G",
-                             "-XX:ParallelGCThreads=1"
+                             "-XX:ParallelGCThreads=1",
                              "-Djava.compiler=NONE", "-jar",
                              '%stier2/target/tier2-0.0.1-SNAPSHOT-jar-with-dependencies.jar' % (self.sysRootPath),
                              '--cpuEmu', '%d' % (cpuEmu), '--jedisHost', 'localhost'])
