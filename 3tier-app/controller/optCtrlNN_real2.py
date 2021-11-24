993e6ad29bdbc3e5c670d897480c31cb88a36b13
emilio <emilio@mpbwalt.local>
2021-11-24 08:37:41 +0100

    added new model

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -131,9 +131,9 @@ class optCtrlNN3:
         # Ypredicted_N=self.tfmodel({'inputx':input_N,'inputu':Sin})
         # Ypredicted_N=[0,np.zeros([1,2,38]),np.zeros([1,10])]
         
-        # print(self.tfmodel.get_tensor(output_details[0]['index']).shape)
-        # print(self.tfmodel.get_tensor(output_details[1]['index']).shape)
-        # print(self.tfmodel.get_tensor(output_details[2]['index']).shape)
+        print(self.tfmodel.get_tensor(output_details[0]['index']).shape)
+        print(self.tfmodel.get_tensor(output_details[1]['index']).shape)
+        #print(self.tfmodel.get_tensor(output_details[2]['index']).shape)
         
         Bias = self.tfmodel.get_tensor(output_details[2]['index'])
         Gain = self.tfmodel.get_tensor(output_details[1]['index'])
