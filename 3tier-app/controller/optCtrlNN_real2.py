a36e860cf374936352175508c6394635e0c8c882
emilio <emilio@mpbwalt.local>
2021-11-26 17:47:47 +0100

    exp client

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -352,7 +352,7 @@ if __name__ == "__main__":
                 optSNN[:, step] = optU[0:N]
                 tgtStory += [tgt]
                 
-                time.sleep(0.1)
+                #time.sleep(2)
                 
                 # optSPID[:,step]=optSPid
                 # optSPid=mitigateBottleneck(optSPid, Xsim3, tgt)
