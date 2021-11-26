d07c5777f416c54982030890f5ec82e9a53b5263
emilio <emilio@mpbwalt.local>
2021-11-26 13:34:29 +0100

    minor

--- 3tier-app/scripts/initcgv2.sh
+++ 3tier-app/scripts/initcgv2.sh
@@ -8,16 +8,21 @@ mkdir t1
 cd  ./t1
 echo "+cpu" > cgroup.subtree_control
 cat cgroup.subtree_control
-mkdir t1/e1
+mkdir e1
 cd  ./e1
 echo "+cpu" > cgroup.subtree_control
 cat cgroup.subtree_control
 echo "threaded" > cgroup.type
 cat cgroup.type
+
+cd /sys/fs/group
+echo "+cpu" > cgroup.subtree_control
+cat cgroup.subtree_control
 mkdir t2
 cd  ./t2
 echo "+cpu" > cgroup.subtree_control
-mkdir t2/e2
+cat cgroup.subtree_control
+mkdir e2
 cd  ./e2
 echo "+cpu" > cgroup.subtree_control
 cat cgroup.subtree_control
