be93d1b35d9ce304601ff7aa27900e7b1623ea52
emilio <emilio@mpbwalt.local>
2021-11-29 11:02:07 +0100

    minor

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
     #plant=dockersys()
     plant.startSys()
     #plant.startClient(np.random.randint(low=10, high=100))
-    plant.startClient(160,sim=True)
+    plant.startClient(180,sim=True)
     
     #memcached client
     r=Client("localhost:11211")
