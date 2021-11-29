a1ad9170a69ef65d66295d57ef133f31e4d5da13
emilio <emilio@mpbwalt.local>
2021-11-29 15:54:12 +0100

    new model

--- 3tier-app/controller/optCtrlNN_real2.py
+++ 3tier-app/controller/optCtrlNN_real2.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
     dt = 10 ** (-1)
     H = 5
     N = 3
-    rep = 1
+    rep = 30
     drep = 0
     sTime = 10000
     TF = sTime * rep * dt;
@@ -250,7 +250,7 @@ if __name__ == "__main__":
     #plant=dockersys()
     plant.startSys()
     #plant.startClient(np.random.randint(low=10, high=100))
-    plant.startClient(190,sim=True)
+    plant.startClient(195,sim=True)
     
     #memcached client
     r=Client("localhost:11211")
