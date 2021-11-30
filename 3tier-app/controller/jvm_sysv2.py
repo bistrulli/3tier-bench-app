d77aed4c2f89fe3a2e9fe82d932f03290504e304
emilio <emilio@mpbwalt.local>
2021-11-30 07:57:21 +0100

    new model

--- 3tier-app/controller/jvm_sysv2.py
+++ 3tier-app/controller/jvm_sysv2.py
@@ -81,7 +81,7 @@ class jvm_sys(system_interface):
         cpuEmu = 0 if(self.isCpu) else 1
         
         self.sys = []
-        subprocess.Popen(["memcached", "-c", "2048", "-t", "20","-u","root"])
+        subprocess.Popen(["memcached", "-c", "4096", "-t", "80","-u","root"])
         self.waitMemCached()
         self.sys.append(self.findProcessIdByName("memcached")[0])
         
