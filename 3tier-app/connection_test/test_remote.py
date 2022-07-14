464ff232cac8aa5036f422ab9a004d45d212b881
emilio <emilio@mpbwalt.local>
2022-07-14 05:10:59 +0200

    data

--- 3tier-app/connection_test/test_remote.py
+++ 3tier-app/connection_test/test_remote.py
@@ -1,10 +1,21 @@
 from pymemcache.client.base import Client
 import time
+import numpy as np
+from scipy.io import savemat
 
 
 client = Client('monitor')
-for i in range(0,15):
+
+sampleT=[]
+
+nsample=1000
+for i in range(0,nsample):
     st=time.time()
-    client.set('some_key', 'some_value')
-    result = client.get('some_key')
-    print(time.time()-st)
+    client.set('state', "%d"%(np.random.randint(low=0,high=1000)))
+    result = client.get('state')
+    sampleT.append(time.time()-st)
+    print((i*100.0/nsample))
+
+savemat("gketime.mat", {"data":sampleT})
+
+
